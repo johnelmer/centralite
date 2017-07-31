3b54e0fd0e83cca3f996bad234aa1e24a087561f
johnelmer <johnelmer_loretizo@yahoo.com>
2017-08-01 07:18:58 +0800

    chore(All files): Initial Commit

---
+++ app/controllers/subscribers_controller.rb
@@ -0,0 +1,49 @@
+class SubscribersController < ApplicationController
+  def index
+    @subscribers = Subscriber.all
+  end
+
+  def new
+    @subscriber = Subscriber.new()
+  end
+
+  def edit
+    @subscriber = Subscriber.find(params[:id])
+  end
+
+  def show
+    @subscriber = Subscriber.find(params[:id])
+  end
+
+  def create
+    @subscriber = Subscriber.new(subscriber_params)
+    if @subscriber.save
+    
+      redirect_to @subscriber
+    else
+      render 'new'
+    end
+  end
+
+  def destroy
+    @subscriber = Subscriber.find(params[:id])
+    @subscriber.destroy
+
+    redirect_to subscribers_path
+  end
+
+  def update
+    @subscriber = Subscriber.find(params[:id])
+
+    if @subscriber.update(subscriber_params)
+      redirect_to @subscriber
+    else
+      render 'edit'
+    end
+  end
+
+  private
+    def subscriber_params
+      params.require(:subscriber).permit(:firstName, :idNumber)
+    end
+end
---
+++ app/models/subscriber.rb
@@ -0,0 +1,26 @@
+class Subscriber
+  include Mongoid::Document
+  field :lastName, type: String
+  field :firstName, type: String
+  validates_presence_of :firstName, :idNumber
+  field :middleName, type: String
+  field :contactNumber, type: String
+  field :idNumber, type: String
+  field :controlNumber, type: String
+  field :course, type: String
+  field :college, type: String
+  field :email, type: String
+  field :description, type: String
+  field :pictorial, type: Date
+  field :birthday, type: Date
+  field :suffix, type: String
+  field :fathersName, type: String
+  field :mothersName, type: String
+  field :typeOfSubscription, type: String
+  field :isProofreadByLayout, type: Mongoid::Boolean
+  field :isProofreadByManaging, type: Mongoid::Boolean
+  field :isProofreadByAssociate, type: Mongoid::Boolean
+  field :isProofreadByEIC, type: Mongoid::Boolean
+  field :isProofreadByModerator, type: Mongoid::Boolean
+  field :semGraduating, type: String
+end
